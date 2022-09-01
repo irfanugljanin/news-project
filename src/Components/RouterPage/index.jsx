ae273274b3ebfada026e663c0c5677146de91776
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-09-01 21:16:54 +0200

    fix

--- src/Components/RouterPage/index.jsx
+++ src/Components/RouterPage/index.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-
+import Page404 from "../Page404";
 const RouterPage = (props) => {
   return (
     <>
       <Routes>
         <Route path="" element={<p>asd</p>}></Route>
+        <Route path="*" element={<Page404 />}></Route>
       </Routes>
     </>
   );
